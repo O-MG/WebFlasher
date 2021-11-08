738d97834b23feb9d80571bec3e205ad0ea816fa
sygint <sygint@users.noreply.github.com>
2021-11-08 01:45:02 -0800

    Add eslint with basic rules for dead code cleanup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    // "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 13
+    },
+    "rules": {
+        "no-unreachable": "error",
+        "no-unused-vars": "error",
+        "no-constant-condition": "error"
+    }
+};
